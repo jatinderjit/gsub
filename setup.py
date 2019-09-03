2395be7bd686f878868d1a80108670748f64731d
Jatinderjit Singh <jatinderjit89@gmail.com>
2019-09-03 11:44:02 +0530

    Update version

--- setup.py
+++ setup.py
@@ -7,54 +7,34 @@ from setuptools import setup
 setup(
     name="gsub",
     description="Replacement for git-submodule",
-
-    version="0.1.4",
-
+    version="0.1.5",
     author="Amit Upadhyay",
     author_email="code@amitu.com",
-
     url="https://github.com/Coverfox/gsub",
     license="BSD",
-
-
     install_requires=["click==6.6", "six"],
-
-    packages=['gsub'],
+    packages=["gsub"],
     zip_safe=True,
-
-
-    keywords=['git', 'git submodule'],
-
-
+    keywords=["git", "git submodule"],
     classifiers=[
-
-        'Intended Audience :: Developers',
-        'Intended Audience :: End Users/Desktop',
-        'Intended Audience :: System Administrators',
-        'Intended Audience :: Information Technology',
-        'Intended Audience :: Other Audience',
-
-        'Natural Language :: English',
-
-        'Operating System :: OS Independent',
-        'Operating System :: POSIX :: Linux',
-        'Operating System :: Microsoft :: Windows',
-        'Operating System :: MacOS :: MacOS X',
-
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
-
-        'Topic :: Software Development',
-
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: System Administrators",
+        "Intended Audience :: Information Technology",
+        "Intended Audience :: Other Audience",
+        "Natural Language :: English",
+        "Operating System :: OS Independent",
+        "Operating System :: POSIX :: Linux",
+        "Operating System :: Microsoft :: Windows",
+        "Operating System :: MacOS :: MacOS X",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 2",
+        "Programming Language :: Python :: 2.7",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.4",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Topic :: Software Development",
     ],
-    entry_points={
-        'console_scripts': [
-            'gsub = gsub.main:main',
-        ],
-    },
+    entry_points={"console_scripts": ["gsub = gsub.main:main"]},
 )
